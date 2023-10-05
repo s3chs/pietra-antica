f6277bc130105f6f4bb1961b04820d30e758f5f6
sechs <philippe.buteau@skema.edu>
2023-10-05 18:58:21 +0200

    Restart project from scratch. Navbar Done

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div className="navbar-container">
+            <div className="company-name-container">
+                <span>Pietra Antica</span>
+            </div>
+            <div className="navigation-links-container">
+                <a href="#">Taille de pierre</a>
+                <a href="#">Placage et dallage</a>
+                <a href="#">Ravalement de façade</a>
+                <a href="#">Maçonnerie traditionelle</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,9 @@
-'use client';
-import { motion, AnimatePresence} from 'framer-motion';
-import {useState } from 'react';
-import Loader from '@/app/components/Loader/Loader';
-import Banner from '@/app/components/Banner/Banner';
-import Header from '@/app/components/Header/Header';
+import Navbar from '@/app/components/Navbar/Navbar';
 
 export default function Index() {
-
-    const [loading, setLoading] = useState(true);
-
     return (
-        <AnimatePresence>
-            {loading ? (
-                <motion.div key="loader">
-                    <Loader setLoading={setLoading}/>
-                </motion.div>
-            ) : (
-                <>
-                    <Header/>
-                    <Banner/>
-                    {!loading && (
-                        <div className="transition-image final">
-                            <motion.img
-                                src="/images/image-2.jpg"
-                                transition={{ease: [0.6, 0.01, 0.05, 0.9], duration: 1.6}}
-                                layoutId="main-image-1"
-                            />
-                        </div>
-                    )}
-                </>
-            )}
-        </AnimatePresence>
+        <>
+            <Navbar/>
+        </>
     );
 }
