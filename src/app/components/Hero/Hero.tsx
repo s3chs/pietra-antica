ed526d62a46a99251c40b53f2f0ec2913b77956c
sechs <philippe.buteau@skema.edu>
2023-10-05 19:30:24 +0200

    Implemented grid and company name in it

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (
+        <div className="hero-container">
+            <div className="company-first-name"><span className="italic">Pietra</span></div>
+            <div className="company-second-name"><span className="italic">Antica</span></div>
+        </div>
+    );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from '@/app/components/Navbar/Navbar';
+import Hero from '@/app/components/Hero/Hero';
 
 export default function Index() {
     return (
         <>
             <Navbar/>
+            <Hero/>
         </>
     );
 }
