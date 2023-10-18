599cecf0047c3fe30a2f1a8e5f428236bb7e7e20
sechs <philippe.buteau@skema.edu>
2023-10-18 17:36:43 +0200

    Implemented responsive nav

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,16 +1,43 @@
+import { useRef } from 'react';
+
 const Navbar = () => {
+    function displayNav() {
+        navRef.current.classList.toggle('active');
+        overlayRef.current.classList.toggle('active');
+    }
+
+    const navRef = useRef<any>(null);
+    const overlayRef = useRef<any>(null);
+
     return (
-        <div className="navbar-container">
-            <div className="company-name-container">
-                <span>Pietra Antica</span>
+        <>
+            <div className="navbar-container" ref={navRef}>
+                <div className="company-name-container">
+                    <span>Pietra Antica</span>
+                </div>
+                <div className="navigation-links-container">
+                    <a href="#">Taille de pierre</a>
+                    <a href="#">Placage et dallage</a>
+                    <a href="#">Ravalement de façade</a>
+                    <a href="#">Maçonnerie traditionelle</a>
+                </div>
+                <div className="navigation-responsive-container" onClick={displayNav}>
+                    <div className="hamburger-container">
+                        <div className="bar one"></div>
+                        <div className="bar two"></div>
+                        <div className="bar three"></div>
+                    </div>
+                </div>
+                <div className="nav-overlay-container" ref={overlayRef}>
+                    <div className="overlay-nav-links">
+                        <span>Taille de pierre</span>
+                        <span>Placage et dallage</span>
+                        <span>Ravalement de façade</span>
+                        <span>Maçonnerie traditionelle</span>
+                    </div>
+                </div>
             </div>
-            <div className="navigation-links-container">
-                <a href="#">Taille de pierre</a>
-                <a href="#">Placage et dallage</a>
-                <a href="#">Ravalement de façade</a>
-                <a href="#">Maçonnerie traditionelle</a>
-            </div>
-        </div>
+        </>
     );
 };
 
