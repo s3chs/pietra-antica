316740526ee0fc7e607f260e83aa2ddb6a59172b
sechs <philippe.buteau@skema.edu>
2023-11-14 18:43:41 +0100

    Uninstall locomotive scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,8 @@
 'use client';
 import './sass/main.scss';
 import Navbar from '@/app/components/Navbar/Navbar';
-import LocomotiveScroll from 'locomotive-scroll';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-
-    const locomotiveScroll: any = new LocomotiveScroll({
-        lenisOptions: {
-            wrapper: window,
-            content: document.documentElement,
-            lerp: 0.1,
-            duration: 1.2,
-            orientation: 'vertical',
-            gestureOrientation: 'both',
-            smoothWheel: true,
-            smoothTouch: false,
-            wheelMultiplier: 1,
-            touchMultiplier: 2,
-            normalizeWheel: true,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        },
-    });
 
     return (
         <html lang="fr">
